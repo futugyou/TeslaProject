47d6e2fbb8c8fa9206fac8988e636e8988cf766c
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-09-12 17:33:08 +0800

    TN-2 #comment update tesla vehicle user api

---
+++ TeslaApi.Vehicle.Abstractions/IVehicleUser.cs
@@ -0,0 +1,13 @@
+﻿using TeslaApi.Contract.User;
+
+namespace TeslaApi.Vehicle.Abstractions
+{
+    public interface IVehicleUser
+    {
+        Task<PowerwallOrderSessionDataResponse> GetPowerwallOrderSessionData(int id, string token);
+        Task<OnboardingExperienceResponse> GeOnboardingExperience(int id, string token);
+        Task<ServiceSelfSchedulingEligibilityResponse> GetServiceSelfSchedulingEligibility(int id, string token);
+        Task<ReferralDataResponse> GetReferralData(int id, string token);
+        Task<RoadsideAssistanceDataResponse> GetRoadsideAssistanceData(int id, string token);
+    }
+}
