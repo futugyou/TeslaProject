588aaf8ab0f5ead2f3397676fb23ca74ce7d5600
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-09 13:36:39 +0000

    add teslaApi user abstractions

---
+++ TeslaApi.User.Abstractions/UserOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace TeslaApi.User.Abstractions;
+
+public class UserOptions
+{
+    public string Me { get; set; } = "/api/1/users/me";
+    public string VaultProfile { get; set; } = "/api/1/users/vault_profile";
+    public string FeatureConfig { get; set; } = "/api/1/users/feature_config";
+    public string UserKeys { get; set; } = "/api/1/users/keys";
+
+}
