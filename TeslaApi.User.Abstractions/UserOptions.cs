0fc9b526d9209fb42cc13245e4b54f6353fb9218
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-10 03:17:37 +0000

    implement user interface

--- TeslaApi.User.Abstractions/UserOptions.cs
+++ TeslaApi.User.Abstractions/UserOptions.cs
@@ -2,9 +2,9 @@
 
 public class UserOptions
 {
+    public string TeslaBaseUrl { get; set; } = "https://owner-api.teslamotors.com";
     public string Me { get; set; } = "/api/1/users/me";
     public string VaultProfile { get; set; } = "/api/1/users/vault_profile";
     public string FeatureConfig { get; set; } = "/api/1/users/feature_config";
     public string UserKeys { get; set; } = "/api/1/users/keys";
-
 }
---
+++ TeslaApi.User/User.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using TeslaApi.Contract;
+using TeslaApi.Contract.User;
+using TeslaApi.User.Abstractions;
+
+namespace TeslaApi.User;
+
+public class User : IUser
+{
+    private readonly UserOptions _options;
+    private readonly HttpClient httpClient;
+
+    public User(IOptionsMonitor<UserOptions> options,
+        IHttpClientFactory clientFactory)
+    {
+        options = options ?? throw new ArgumentNullException(nameof(options));
+        clientFactory = clientFactory ?? throw new ArgumentNullException(nameof(clientFactory));
+
+        _options = options.CurrentValue;
+        httpClient = clientFactory.CreateClient(TeslaApiConst.TESLA_SERVICE_HTTPCLIENT_NAME);
+        httpClient.BaseAddress = new Uri(_options.TeslaBaseUrl);
+    }
+
+    public Task<BluetoothKeysResponse> BluetoothKeys(BluetoothKeysRequest request, string token)
+    {
+        return httpClient.UtilsPostAsync<BluetoothKeysRequest, BluetoothKeysResponse>(request, _options.UserKeys, token);
+    }
+
+    public Task<FeatureConfigResponse> FeatureConfig(string token)
+    {
+        return httpClient.UtilsGetAsync<FeatureConfigResponse>(_options.FeatureConfig, token);
+    }
+
+    public Task<MeReponse> UserInformation(string token)
+    {
+        return httpClient.UtilsGetAsync<MeReponse>(_options.Me, token);
+    }
+
+    public Task<VaultProfileResponse> VaultProfile(string token)
+    {
+        return httpClient.UtilsGetAsync<VaultProfileResponse>(_options.VaultProfile, token);
+    }
+}
