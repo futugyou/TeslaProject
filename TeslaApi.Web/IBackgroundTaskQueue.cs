fb36ed62347f06c5aafab701bb0d8ce6c1aba9cf
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-12 10:14:28 +0000

    queued background tasks

---
+++ TeslaApi.Web/IBackgroundTaskQueue.cs
@@ -0,0 +1,47 @@
+using System.Threading.Channels;
+
+namespace TeslaApi.Web;
+
+/// <summary>
+/// https://learn.microsoft.com/zh-cn/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-8.0&tabs=visual-studio#queued-background-tasks
+/// </summary>
+public interface IBackgroundTaskQueue
+{
+    ValueTask QueueBackgroundWorkItemAsync(Func<IServiceProvider, CancellationToken, ValueTask> workItem);
+
+    ValueTask<Func<IServiceProvider, CancellationToken, ValueTask>> DequeueAsync(IServiceProvider sp, CancellationToken cancellationToken);
+}
+
+public class BackgroundTaskQueue : IBackgroundTaskQueue
+{
+    private readonly Channel<Func<IServiceProvider, CancellationToken, ValueTask>> _queue;
+
+    public BackgroundTaskQueue(int capacity)
+    {
+        // Capacity should be set based on the expected application load and
+        // number of concurrent threads accessing the queue.            
+        // BoundedChannelFullMode.Wait will cause calls to WriteAsync() to return a task,
+        // which completes only when space became available. This leads to backpressure,
+        // in case too many publishers/calls start accumulating.
+        var options = new BoundedChannelOptions(capacity)
+        {
+            FullMode = BoundedChannelFullMode.Wait
+        };
+        Console.WriteLine(options.SingleReader + ""+options.SingleWriter + ""+ options.AllowSynchronousContinuations + ""+options.Capacity);
+        _queue = Channel.CreateBounded<Func<IServiceProvider, CancellationToken, ValueTask>>(options);
+    }
+
+    public async ValueTask QueueBackgroundWorkItemAsync(Func<IServiceProvider, CancellationToken, ValueTask> workItem)
+    {
+        ArgumentNullException.ThrowIfNull(workItem);
+        Console.WriteLine(_queue.Reader.Count);
+        await _queue.Writer.WriteAsync(workItem);
+    }
+
+    public async ValueTask<Func<IServiceProvider, CancellationToken, ValueTask>> DequeueAsync(IServiceProvider sp, CancellationToken cancellationToken)
+    {
+        var workItem = await _queue.Reader.ReadAsync(cancellationToken);
+
+        return workItem;
+    }
+}
