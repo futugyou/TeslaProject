65cdd7bb7d39ea7d455c066c24edb8aed5874562
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-18 11:25:47 +0000

    convert token string to token info

--- src/Sdk/TeslaApi.Contract/TokenParse .cs
+++ src/Sdk/TeslaApi.Contract/TokenParse .cs
@@ -4,6 +4,7 @@ namespace TeslaApi.Contract;
 
 public class TokenParse
 {
+    public static DateTime UnixStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
     public static TokenLocal CheckTokenLocal(string accessToken)
     {
         // Decode the access token
@@ -15,6 +16,8 @@ public class TokenParse
             {
                 return TokenLocal.Unkown;
             }
+            DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime date = start.AddSeconds(1111).ToLocalTime();
 
             var cn = jsonToken.Claims.Where(p => p.Type == "iss" && p.Value.StartsWith("https://auth.tesla.cn"));
             if (cn != null && cn.Any())
@@ -27,9 +30,9 @@ public class TokenParse
             return TokenLocal.Unkown;
         }
 
-
         return TokenLocal.Global;
     }
+
 }
 
 public enum TokenLocal
@@ -37,4 +40,53 @@ public enum TokenLocal
     Global,
     China,
     Unkown,
+}
+
+public class TokenInfo
+{
+    public string Subject { get; }
+    public string Issue { get; }
+    public string AuthorizedParty { get; }
+    public DateTime ExpirationTime { get; }
+    public DateTime IssuedTime { get; }
+    public TokenLocal Locale { get; }
+
+    public TokenInfo(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+
+        try
+        {
+            if (handler.ReadToken(token) is not JwtSecurityToken jsonToken)
+            {
+                return;
+            }
+
+            Subject = jsonToken.Subject;
+            Issue = jsonToken.Issuer;
+            IssuedTime = jsonToken.IssuedAt;
+
+            foreach (var claim in jsonToken.Claims)
+            {
+                if (claim.Type == "azp")
+                {
+                    AuthorizedParty = claim.Value;
+                }
+                if (claim.Type == "exp")
+                {
+                    ExpirationTime = TokenParse.UnixStart.AddSeconds(long.Parse(claim.Value)).ToLocalTime();
+                }
+                if (claim.Type == "ou_code")
+                {
+                    Locale = claim.Value == "CN" ? TokenLocal.China : TokenLocal.Global;
+                }
+            }
+        }
+        catch
+        {
+
+        }
+    }
+
+    public bool IsTokenExpiration() => IssuedTime.CompareTo(DateTime.UtcNow.AddSeconds(-5)) < 0;
 }
