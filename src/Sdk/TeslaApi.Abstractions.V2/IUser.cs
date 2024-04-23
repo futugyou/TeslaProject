20b728e3a12b51d2513907e6ab8e6d9a3881149b
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-23 10:49:39 +0000

    tesla use with refit

---
+++ src/Sdk/TeslaApi.Abstractions.V2/IUser.cs
@@ -0,0 +1,19 @@
+using Refit;
+using TeslaApi.Contract.User;
+
+namespace TeslaApi.Abstractions.V2;
+
+public interface IUser
+{
+    [Get("/api/1/users/me")]
+    Task<MeReponse> UserInformation([Authorize("Bearer")] string token);
+
+    [Get("/api/1/users/vault_profile")]
+    Task<VaultProfileResponse> VaultProfile([Authorize("Bearer")] string token);
+
+    [Get("/api/1/users/feature_config")]
+    Task<FeatureConfigResponse> FeatureConfig([Authorize("Bearer")] string token);
+
+    [Post("/api/1/users/keys")]
+    Task<BluetoothKeysResponse> BluetoothKeys(BluetoothKeysRequest request, [Authorize("Bearer")] string token);
+}
