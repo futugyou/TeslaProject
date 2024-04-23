ab0b02ca02285fa5c60ccd426c29809dfbae778c
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-23 10:23:57 +0000

    use refit to build v2 sdk

---
+++ src/Sdk/TeslaApi.Abstractions.V2/ITeslaAuthentication.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using TeslaApi.Contract.Authentication;
+
+namespace TeslaApi.Abstractions.V2;
+
+public interface ITeslaAuthentication
+{
+    Task<string> GetAuthorizeEndPoint(AuthorizeEndPointRequest request)
+    {
+        return Task.FromResult("https://auth.tesla.com/oauth2/v3/authorize?" + request.ConverToUri());
+    }
+
+    [Post("/oauth2/v3/token")]
+    Task<AccessTokenResponse> GetAccessToken(AccessTokenRequest request);
+    [Post("/oauth2/v3/token")]
+    Task<AccessTokenResponse> RefreshBearerToken(RefreshTokenRequest request);
+}
