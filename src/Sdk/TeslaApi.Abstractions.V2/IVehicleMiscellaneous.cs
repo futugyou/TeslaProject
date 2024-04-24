493a73674cf59f99ba80734962cf38f67b9b32fb
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-24 07:50:52 +0000

    refit vehicle miscellaneous api

---
+++ src/Sdk/TeslaApi.Abstractions.V2/IVehicleMiscellaneous.cs
@@ -0,0 +1,10 @@
+using TeslaApi.Contract.Vehicle.Miscellaneous;
+using Refit;
+
+namespace TeslaApi.Abstractions;
+
+public interface IVehicleMiscellaneous
+{
+    [Get("/api/1/vehicles/{vehicle_id}/release_notes")]
+    Task<ReleaseNotesResponse> ReleaseNote(string vehicle_id, [Authorize("Bearer")] string token);
+}
