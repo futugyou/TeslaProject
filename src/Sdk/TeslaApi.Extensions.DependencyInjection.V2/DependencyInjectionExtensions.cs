53f100aab73e821c1491a3915a22870d11bc4454
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-24 09:26:07 +0000

    registe refit client and stream

--- src/Sdk/TeslaApi.Extensions.DependencyInjection.V2/DependencyInjectionExtensions.cs
+++ src/Sdk/TeslaApi.Extensions.DependencyInjection.V2/DependencyInjectionExtensions.cs
@@ -21,18 +21,30 @@ public static class DependencyInjectionExtensions
     {
         ArgumentNullException.ThrowIfNull(services);
         ArgumentNullException.ThrowIfNull(configuration);
-        
+
         services.AddTransient<EndpointChangeHandler>();
 
-        services.AddRefitClient< ITeslaAuthentication>()
+        services.AddRefitClient<ITeslaAuthentication>()
             .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://auth.tesla.com"));
 
+        var vehicleInterfaces = typeof(IVehicleRefit).Assembly.GetTypes()
+                    .Where(t => t.IsInterface && typeof(IVehicleRefit).IsAssignableFrom(t) && t != typeof(IVehicleRefit))
+                    .ToList();
 
-        var clientBuilder = services.AddRefitClient<IUser>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://owner-api.teslamotors.com"));
+        foreach (var T in vehicleInterfaces)
+        {
+            var genericMethod = typeof(Refit.HttpClientFactoryExtensions).GetMethod("AddRefitClient", [typeof(IServiceCollection), typeof(RefitSettings)])?.MakeGenericMethod(T);
+            if (genericMethod?.Invoke(null, [services, null]) is not IHttpClientBuilder builder)
+            {
+                continue;
+            }
 
-        clientBuilderOption?.Invoke(clientBuilder);
-        clientBuilder.AddHttpMessageHandler<EndpointChangeHandler>();
+            builder.ConfigureHttpClient(c => c.BaseAddress = new Uri("https://owner-api.teslamotors.com"));
+            clientBuilderOption?.Invoke(builder);
+            builder.AddHttpMessageHandler<EndpointChangeHandler>();
+        }
+
+        services.AddTransient<ITeslaStream, TeslaStream>();
         return services;
     }
 
