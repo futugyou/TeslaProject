2fd0dbed43b96f4db347a24bb1d0f88feec5d2e9
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-04-25 10:50:44 +0000

    add ws to test api

--- src/Web/TeslaApi.Web/Api/Test.cs
+++ src/Web/TeslaApi.Web/Api/Test.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using TeslaApi.SDK;
 using TeslaApi.Contract;
 using TeslaApi.Contract.Authentication;
+using Extensions;
 
 namespace Api;
 
@@ -16,6 +17,7 @@ public static class TestEndpoints
         vehicleGroup.MapGet("/token", AccessToken).WithName("token");
         vehicleGroup.MapGet("/user", UserInfo).WithName("user");
         vehicleGroup.MapGet("/tokeninfo", LocalInfo).WithName("tokeninfo");
+        vehicleGroup.MapGet("/ws", VehicleWebSocket).WithName("wstest");
     }
 
     static async Task<IResult> AccessToken([FromServices] ITeslaAuthentication tesla, [FromQuery] string code, [FromQuery] string verifier)
@@ -42,4 +44,46 @@ public static class TestEndpoints
         return TypedResults.Ok(result);
     }
 
+    static async Task<IResult> VehicleWebSocket([FromServices] IBackgroundTaskQueue queue, string token, string vid)
+    {
+        StreamRequest rquest = new()
+        {
+            VinID = int.Parse(vid),
+            Token = token,
+        };
+        var pararmeter = new TaskQueueParameter
+        {
+            IdentityKey = vid,
+            Parameter = rquest,
+        };
+        // send to queue
+        await queue.QueueBackgroundWorkItemAsync(ConnectVehicleStream, pararmeter);
+        return TypedResults.Ok();
+    }
+
+    static async ValueTask ConnectVehicleStream(IServiceProvider sp, object state, CancellationToken stoppingToken)
+    {
+        using var scope = sp.CreateScope();
+        var teslaStream = scope.ServiceProvider.GetRequiredService<ITeslaStream>();
+        var _logger = scope.ServiceProvider.GetRequiredService<ILogger<ITeslaStream>>();
+
+        StreamRequest rquest = (StreamRequest)state;
+        try
+        {
+            await teslaStream.StartAsync(rquest, stoppingToken);
+            while (await teslaStream.MessageReader.WaitToReadAsync(stoppingToken))
+            {
+                var msg = await teslaStream.MessageReader.ReadAsync(stoppingToken);
+                _logger.LogInformation("test {Message}", msg?.Value);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("{Message}", ex.Message);
+        }
+        finally
+        {
+            await teslaStream.StopAsync(stoppingToken);
+        }
+    }
 }
