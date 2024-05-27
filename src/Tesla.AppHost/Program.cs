d049f0044773511bc1d620eb5ca443edbd417ac8
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-05-27 02:15:41 +0000

    add redis

--- src/Tesla.AppHost/Program.cs
+++ src/Tesla.AppHost/Program.cs
@@ -1,3 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var redis = builder.AddRedis("redis");
+
+// Service consumption
+builder.AddProject<Projects.Tealse_Charge>("charge")
+       .WithReference(redis);
+builder.AddProject<Projects.Tealse_Dashboards>("dashboards")
+       .WithReference(redis);
+builder.AddProject<Projects.Tealse_Journey>("journey")
+       .WithReference(redis);
+builder.AddProject<Projects.Tealse_Stream>("stream")
+       .WithReference(redis);
+builder.AddProject<Projects.Tealse_User>("user")
+       .WithReference(redis);
+builder.AddProject<Projects.Tealse_Vehicle>("vehicle")
+       .WithReference(redis);
+
 builder.Build().Run();
