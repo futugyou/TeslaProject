37d31df2bc04b8085373d3bb724028c9b5df5c6b
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-06-02 04:18:32 +0000

    remove unuse code

--- src/Tesla.Sdk/TeslaApi.Abstractions/IVehicleMiscellaneous.cs
+++
@@ -1,8 +0,0 @@
-using TeslaApi.Contract.Vehicle.Miscellaneous;
-
-namespace TeslaApi.Abstractions;
-
-public interface IVehicleMiscellaneous
-{
-    Task<ReleaseNotesResponse> ReleaseNote(string vehicle_id, string token);
-}
--- src/Tesla.Sdk/TeslaApi.Storage.Abstractions/ITeslaUserAuthInfoRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace TeslaApi.Storage.Abstractions;
-
-public interface ITeslaUserAuthInfoRepository
-{
-    Task SaveTeslaUserAuthInfo(TeslaUserAuthInfo info);
-    Task<TeslaUserAuthInfo> GetTeslaUserAuthInfoByVin(string vin);
-    Task<TeslaUserAuthInfo> GetTeslaUserAuthInfoByVehicleId(string vehicleId);
-    Task<TeslaUserAuthInfo> GetTeslaUserAuthInfoByUserId(string userId);
-}
