e0da7ca3c425940a33312f15464fd340943d0c91
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-06-05 13:03:24 +0000

    update deserialization method

--- src/Web/TeslaApi.Web/Services/VehicleMessageConsumer.cs
+++ src/Web/TeslaApi.Web/Services/VehicleMessageConsumer.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Domain;
 using MassTransit;
 
@@ -27,131 +28,154 @@ public class VehicleStreamData
     /// <summary>
     /// Vehicle speed in kilometers per hour.
     /// </summary>
+    [DataPosition(0)]
     public double Speed { get; set; }
 
     /// <summary>
     /// The vehicle's odometer reading in kilometers.
     /// </summary>
+    [DataPosition(1)]
     public int Odometer { get; set; }
 
     /// <summary>
     /// State of Charge of the vehicle's battery, in percentage.
     /// </summary>
+    [DataPosition(2)]
     public double SOC { get; set; }
 
     /// <summary>
     /// Elevation above sea level in meters.
     /// </summary>
+    [DataPosition(3)]
     public double Elevation { get; set; }
 
     /// <summary>
     /// Estimated heading of the vehicle in degrees from North.
     /// </summary>
+    [DataPosition(4)]
     public double EstHeading { get; set; }
 
     /// <summary>
     /// Estimated latitude of the vehicle in decimal degrees.
     /// </summary>
+    [DataPosition(5)]
     public double EstLat { get; set; }
 
     /// <summary>
     /// Estimated longitude of the vehicle in decimal degrees.
     /// </summary>
+    [DataPosition(6)]
     public double EstLng { get; set; }
 
     /// <summary>
     /// Corrected estimated latitude of the vehicle after adjustments.
     /// </summary>
+    [DataPosition(7)]
     public double EstCorrectedLat { get; set; }
 
     /// <summary>
     /// Corrected estimated longitude of the vehicle after adjustments.
     /// </summary>
+    [DataPosition(8)]
     public double EstCorrectedLng { get; set; }
 
     /// <summary>
     /// Native GPS latitude of the vehicle.
     /// </summary>
+    [DataPosition(9)]
     public double NativeLatitude { get; set; }
 
     /// <summary>
     /// Native GPS longitude of the vehicle.
     /// </summary>
+    [DataPosition(10)]
     public double NativeLongitude { get; set; }
 
     /// <summary>
     /// Native heading of the vehicle based on GPS data in degrees.
     /// </summary>
+    [DataPosition(11)]
     public double NativeHeading { get; set; }
 
     /// <summary>
     /// Type of the native location system used.
     /// </summary>
+    [DataPosition(12)]
     public string NativeType { get; set; }
 
     /// <summary>
     /// Indicates whether the native location is supported.
     /// </summary>
+    [DataPosition(13)]
     public bool NativeLocationSupported { get; set; }
 
     /// <summary>
     /// Power output of the vehicle in kilowatts.
     /// </summary>
+    [DataPosition(14)]
     public double Power { get; set; }
 
     /// <summary>
     /// Current shift state of the vehicle (e.g., park, drive, reverse).
     /// </summary>
+    [DataPosition(15)]
     public string ShiftState { get; set; }
 
     /// <summary>
     /// The current range of the vehicle on a full charge, in kilometers.
     /// </summary>
+    [DataPosition(16)]
     public int Range { get; set; }
 
     /// <summary>
     /// The estimated range of the vehicle based on current driving conditions, in kilometers.
     /// </summary>
+    [DataPosition(17)]
     public int EstRange { get; set; }
 
     /// <summary>
     /// The vehicle's current heading in degrees from North.
     /// </summary>
+    [DataPosition(18)]
     public double Heading { get; set; }
 
     public static VehicleStreamData ParseVehicleStreamData(string data)
     {
         // Split the string by the comma delimiter
         string[] parts = data.Split(',');
+        VehicleStreamData vehicleData = new();
 
-        // Create a new instance of VehicleStreamData
-        VehicleStreamData vehicleData = new()
+        // Get the properties of VehicleData
+        var properties = typeof(VehicleStreamData).GetProperties();
+
+        foreach (var property in properties)
         {
-            // Assign each part to the corresponding property of VehicleStreamData
-            // Note: You need to convert each part to the proper data type
-            Speed = double.Parse(parts[0]),
-            Odometer = int.Parse(parts[1]),
-            SOC = double.Parse(parts[2]),
-            Elevation = double.Parse(parts[3]),
-            EstHeading = double.Parse(parts[4]),
-            EstLat = double.Parse(parts[5]),
-            EstLng = double.Parse(parts[6]),
-            EstCorrectedLat = double.Parse(parts[7]),
-            EstCorrectedLng = double.Parse(parts[8]),
-            NativeLatitude = double.Parse(parts[9]),
-            NativeLongitude = double.Parse(parts[10]),
-            NativeHeading = double.Parse(parts[11]),
-            NativeType = parts[12],
-            NativeLocationSupported = bool.Parse(parts[13]),
-            Power = double.Parse(parts[14]),
-            ShiftState = parts[15],
-            Range = int.Parse(parts[16]),
-            EstRange = int.Parse(parts[17]),
-            Heading = double.Parse(parts[18])
-        };
+            // Get the DataPosition attribute if it exists
+            var attribute = property.GetCustomAttribute<DataPositionAttribute>();
+            if (attribute != null)
+            {
+                int position = attribute.Position;
 
-        // Return the populated VehicleData instance
+                // Convert the part at the given position to the property type and set the value
+                if (position < parts.Length)
+                {
+                    object value = Convert.ChangeType(parts[position], property.PropertyType);
+                    property.SetValue(vehicleData, value);
+                }
+            }
+        }
+
         return vehicleData;
     }
 }
 
+[AttributeUsage(AttributeTargets.Property)]
+public class DataPositionAttribute : Attribute
+{
+    public int Position { get; private set; }
+
+    public DataPositionAttribute(int position)
+    {
+        Position = position;
+    }
+}
