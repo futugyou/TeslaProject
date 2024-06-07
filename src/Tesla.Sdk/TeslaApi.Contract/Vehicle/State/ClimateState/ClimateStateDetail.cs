9eefe4b52d8ad0606048a311cf4f4c335f524f6b
futugyousuzu <17871902+futugyou@users.noreply.github.com>
2024-06-07 14:51:42 +0000

    update tesla sdk contract

--- src/Tesla.Sdk/TeslaApi.Contract/Vehicle/State/ClimateState/ClimateStateDetail.cs
+++ src/Tesla.Sdk/TeslaApi.Contract/Vehicle/State/ClimateState/ClimateStateDetail.cs
@@ -5,31 +5,31 @@ namespace TeslaApi.Contract.Vehicle.State.ClimateState;
 public class ClimateStateDetail
 {
     [JsonPropertyName("battery_heater")]
-    public bool? BatteryHeater { get; set; }
+    public bool BatteryHeater { get; set; }
     [JsonPropertyName("battery_heater_no_power")]
-    public object BatteryHeaterNoPower { get; set; }
+    public bool BatteryHeaterNoPower { get; set; }
     [JsonPropertyName("climate_keeper_mode")]
     public string ClimateKeeperMode { get; set; }
     [JsonPropertyName("defrost_mode")]
-    public int? DefrostMode { get; set; }
+    public int DefrostMode { get; set; }
     [JsonPropertyName("driver_temp_setting")]
     public float DriverTempSetting { get; set; }
     [JsonPropertyName("fan_status")]
-    public int? FanStatus { get; set; }
+    public int FanStatus { get; set; }
     [JsonPropertyName("inside_temp")]
     public float InsideTemp { get; set; }
     [JsonPropertyName("is_auto_conditioning_on")]
-    public bool? IsAutoConditioningOn { get; set; }
+    public bool IsAutoConditioningOn { get; set; }
     [JsonPropertyName("is_climate_on")]
-    public bool? IsClimateOn { get; set; }
+    public bool IsClimateOn { get; set; }
     [JsonPropertyName("is_front_defroster_on")]
-    public bool? IsFrontDefrosterOn { get; set; }
+    public bool IsFrontDefrosterOn { get; set; }
     [JsonPropertyName("is_preconditioning")]
-    public bool? IsPreconditioning { get; set; }
+    public bool IsPreconditioning { get; set; }
     [JsonPropertyName("is_rear_defroster_on")]
-    public bool? IsRearDefrosterOn { get; set; }
+    public bool IsRearDefrosterOn { get; set; }
     [JsonPropertyName("left_temp_direction")]
-    public int? LeftTempDirection { get; set; }
+    public int LeftTempDirection { get; set; }
     [JsonPropertyName("max_avail_temp")]
     public float MaxAvailTemp { get; set; }
     [JsonPropertyName("min_avail_temp")]
@@ -39,17 +39,17 @@ public class ClimateStateDetail
     [JsonPropertyName("passenger_temp_setting")]
     public float PassengerTempSetting { get; set; }
     [JsonPropertyName("remote_heater_control_enabled")]
-    public bool? RemoteHeaterControlEnabled { get; set; }
+    public bool RemoteHeaterControlEnabled { get; set; }
     [JsonPropertyName("right_temp_direction")]
-    public int? RightTempDirection { get; set; }
+    public int RightTempDirection { get; set; }
     [JsonPropertyName("seat_heater_left")]
-    public int? SeatHeaterLeft { get; set; }
+    public int SeatHeaterLeft { get; set; }
     [JsonPropertyName("seat_heater_right")]
-    public int? SeatHeaterRight { get; set; }
+    public int SeatHeaterRight { get; set; }
     [JsonPropertyName("side_mirror_heaters")]
-    public bool? SideMirrorHeaters { get; set; }
+    public bool SideMirrorHeaters { get; set; }
     [JsonPropertyName("timestamp")]
     public long Timestamp { get; set; }
     [JsonPropertyName("wiper_blade_heater")]
-    public bool? WiperBladeHeater { get; set; }
+    public bool WiperBladeHeater { get; set; }
 }
